e101ce19512d1c1d9f1250ad94a244d057f0b4c6
abadug5793 <bavani7777@gmail.com>
2018-06-20 15:03:17 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -30,7 +30,7 @@ end
 def new_animal_accumulation(number_of_puppies, number_of_cats)
   if number_of_puppies == 0 || number_of_cats == 0
     puts "Good Job!"
-  else number_of_puppies > = 1 || number_of_cats > = 1
+  else number_of_puppies > 1 || number_of_cats > 1
     puts "Slow Down"
   end
 end
